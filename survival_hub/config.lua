6441f31ebc2b2c8075a5ac8fc9fdd7bee2cfe3b3
GESUS <105702323+DevAlexandre0@users.noreply.github.com>
2025-09-25 14:44:17 +0700

    Update config.lua

--- survival_hub/config.lua
+++ survival_hub/config.lua
@@ -1,32 +1,41 @@
--- FILE: survival_hub/config.lua
 Config = {}
 
--- Qbox-only
 Config.Debug = true
-Config.LogPrefix = '[survival_hub:qbox]'
+Config.LogPrefix = '[survival_hub]'
 Config.BroadcastEnabled = true
 
--- namespace
+Config.FrameworkPriority = { 'qbox', 'qb', 'esx', 'standalone' }
 Config.Namespaces = { 'movement', 'needs', 'health', 'bio', 'env' }
 
--- Clamp
 Config.Clamps = {
-  movement = { stamina={0,100}, oxygen={0,100} },
-  needs    = { food={0,100}, water={0,100}, energy={0,100}, stress={0,100}, poop={0,100}, pee={0,100}, stamina={0,100}, oxygen={0,100} },
-  health   = { hp={0,100}, blood={0,100}, bleeding={0,100}, fracture={0,1}, pain={0,100} },
-  bio      = { infection={0,100}, parasite={0,100}, disease={0,100}, immunity={0,100}, metabolism={0,100}, stomach={0,100} },
-  env      = { temp_body={30,43}, temp_env={-20,60}, wet={0,100}, rad={0,100} }
+    movement = { stamina = {0, 100}, oxygen = {0, 100}, speed = {0, 150} },
+    needs = {
+        food = {0, 100}, water = {0, 100}, energy = {0, 100}, stress = {0, 100},
+        poop = {0, 100}, pee = {0, 100}
+    },
+    health = {
+        hp = {0, 100}, blood = {0, 100}, bleed = {0, 3}, fracture = {0, 1},
+        pain = {0, 100}, trauma = {0, 100}
+    },
+    bio = {
+        infection = {0, 100}, parasite = {0, 100}, disease = {0, 100},
+        immunity = {0, 100}, metabolism = {0, 100}, stomach = {0, 100}
+    },
+    env = {
+        temp_env = {-20, 60}, temp_body = {30, 43}, wetness = {0, 100},
+        wind = {0, 100}, precip = {0, 100}, radiation = {0, 100}
+    }
 }
 
--- Rate limit / namespace
 Config.Rate = {
-  windowMs = 1000,
-  maxMsgs = 20,
-  maxPayloadBytes = 2048
+    windowMs = 1000,
+    maxMsgs = 20,
+    maxPayloadBytes = 2048
 }
 
--- Persistence (citizenid for key)
 Config.Persistence = {
-  table = 'survival_states',
-  flushIntervalSec = 30
+    enabled = true,
+    table = 'survival_states',
+    flushIntervalSec = 30,
+    fileFallbackPrefix = 'survival_state_'
 }
